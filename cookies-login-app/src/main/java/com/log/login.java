e1a32ddf5f6295e2e45de03de6c883bf77c61be2
vishvjeet <vishvjeetsinghrathore751@gmail.com>
2023-10-06 10:39:56 +0530

    initial commit

---
+++ cookies-login-app/src/main/java/com/log/login.java
@@ -0,0 +1,38 @@
+package com.log;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/userlogin")
+public class login extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String email=req.getParameter("em");
+		String password =req.getParameter("ps");
+		
+		Cookie ck =new Cookie("em", email);
+		Cookie ck2 =new Cookie("ps", password);
+	
+		
+		resp.addCookie(ck);
+		resp.addCookie(ck2);
+		
+		resp.sendRedirect("profile");
+		
+		
+//		
+				
+	}
+
+	
+
+}
